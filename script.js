c0622fd084d108aee155a263d81116d8903a9a02
Samuel Silveira <samuel.silveira@live.com>
2026-09-11 09:51:10 -0300

    Hover effect set on squares

--- script.js
+++ script.js
@@ -4,4 +4,6 @@ for (let i = 0; i < (16 * 16); i++) {
     const square = document.createElement("div");
     square.setAttribute("class", "square");
     container.appendChild(square);
+    square.addEventListener("mouseenter", (e) => e.target.style.backgroundColor = "green");
+    square.addEventListener("mouseleave", (e) => e.target.style.backgroundColor = "lightgreen");
 };
